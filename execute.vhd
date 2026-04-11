7b80893e10d4a8209c1de302082d8e55b01553de
Rachel Ruddy <82718260+rachelruddy@users.noreply.github.com>
2026-04-11 09:19:11 -0400

    merging execute and memory from sara's fork

--- execute.vhd
+++ execute.vhd
@@ -1,15 +1,169 @@
 LIBRARY ieee;
-USE ieee.std_logic_1164.all;
-USE ieee.numeric_std.all;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
 
+ENTITY execute IS
+    PORT (
+        clk : IN STD_LOGIC;
+        reset : IN STD_LOGIC;
+        A : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        B : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        Imm : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        IR : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        NPC : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        ALUSrc : IN STD_LOGIC;
+        ALUFunc : IN STD_LOGIC_VECTOR(4 DOWNTO 0);
+        AUSrc : IN STD_LOGIC;
+        Branch : IN STD_LOGIC;
+        BranchType : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+        Jump : IN STD_LOGIC;
+        JumpReg : IN STD_LOGIC;
+        MemRead : IN STD_LOGIC;
+        MemWrite : IN STD_LOGIC;
+        RegWrite : IN STD_LOGIC;
+        MemToReg : IN STD_LOGIC;
 
-entity execute is
-    port(
-        clk : in STD_LOGIC;
-        reset : in STD_LOGIC
+        ALUResult_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+        B_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+        IR_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+        NPC_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+        BranchTaken : OUT STD_LOGIC;
+        BranchTarget : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+
+        MemRead_out : OUT STD_LOGIC;
+        MemWrite_out : OUT STD_LOGIC;
+        RegWrite_out : OUT STD_LOGIC;
+        MemToReg_out : OUT STD_LOGIC
     );
-end execute;
+END execute;
 
-architecture rtl of execute is
-begin
-end rtl;
+ARCHITECTURE rtl OF execute IS
+
+    COMPONENT alu IS
+        PORT (
+            A : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+            B : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+            ALUFunc : IN STD_LOGIC_VECTOR(4 DOWNTO 0);
+            Result : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+            Zero : OUT STD_LOGIC;
+            Negative : OUT STD_LOGIC;
+            Carry : OUT STD_LOGIC;
+            Overflow : OUT STD_LOGIC
+        );
+    END COMPONENT;
+
+    CONSTANT BR_BEQ : STD_LOGIC_VECTOR(2 DOWNTO 0) := "000";
+    CONSTANT BR_BNE : STD_LOGIC_VECTOR(2 DOWNTO 0) := "001";
+    CONSTANT BR_BLT : STD_LOGIC_VECTOR(2 DOWNTO 0) := "010";
+    CONSTANT BR_BGE : STD_LOGIC_VECTOR(2 DOWNTO 0) := "011";
+    CONSTANT BR_BLTU : STD_LOGIC_VECTOR(2 DOWNTO 0) := "100";
+    CONSTANT BR_BGEU : STD_LOGIC_VECTOR(2 DOWNTO 0) := "101";
+
+    SIGNAL alu_in_A : STD_LOGIC_VECTOR(31 DOWNTO 0);
+    SIGNAL alu_in_B : STD_LOGIC_VECTOR(31 DOWNTO 0);
+
+    SIGNAL alu_result : STD_LOGIC_VECTOR(31 DOWNTO 0);
+    SIGNAL alu_zero : STD_LOGIC;
+    SIGNAL alu_negative : STD_LOGIC;
+    SIGNAL alu_carry : STD_LOGIC;
+    SIGNAL alu_overflow : STD_LOGIC;
+
+    SIGNAL branch_cond : STD_LOGIC;
+    SIGNAL branch_taken : STD_LOGIC;
+    SIGNAL branch_target : STD_LOGIC_VECTOR(31 DOWNTO 0);
+
+    SIGNAL lt_signed : STD_LOGIC;
+    SIGNAL lt_unsigned : STD_LOGIC;
+
+BEGIN
+
+    alu_in_A <= NPC WHEN AUSrc = '1' ELSE
+        A;
+
+    alu_in_B <= Imm WHEN ALUSrc = '1' ELSE
+        B;
+
+    alu_inst : alu
+    PORT MAP(
+        A => alu_in_A,
+        B => alu_in_B,
+        ALUFunc => ALUFunc,
+        Result => alu_result,
+        Zero => alu_zero,
+        Negative => alu_negative,
+        Carry => alu_carry,
+        Overflow => alu_overflow
+    );
+
+    lt_signed <= alu_negative XOR alu_overflow;
+    lt_unsigned <= NOT alu_carry;
+
+    PROCESS (Branch, BranchType, alu_zero, lt_signed, lt_unsigned)
+    BEGIN
+        CASE BranchType IS
+            WHEN BR_BEQ => branch_cond <= alu_zero;
+            WHEN BR_BNE => branch_cond <= NOT alu_zero;
+            WHEN BR_BLT => branch_cond <= lt_signed;
+            WHEN BR_BGE => branch_cond <= NOT lt_signed;
+            WHEN BR_BLTU => branch_cond <= lt_unsigned;
+            WHEN BR_BGEU => branch_cond <= NOT lt_unsigned;
+            WHEN OTHERS => branch_cond <= '0';
+        END CASE;
+    END PROCESS;
+
+    PROCESS (Branch, branch_cond, Jump, JumpReg, A, Imm, NPC)
+        VARIABLE jalr_raw : STD_LOGIC_VECTOR(31 DOWNTO 0);
+    BEGIN
+        IF Jump = '1' THEN
+            branch_taken <= '1';
+            branch_target <= STD_LOGIC_VECTOR(
+                UNSIGNED(NPC) + UNSIGNED(Imm));
+
+        ELSIF JumpReg = '1' THEN
+            jalr_raw := STD_LOGIC_VECTOR(
+                SIGNED(A) + SIGNED(Imm));
+            jalr_raw(0) := '0';
+            branch_taken <= '1';
+            branch_target <= jalr_raw;
+
+        ELSIF Branch = '1' AND branch_cond = '1' THEN
+            branch_taken <= '1';
+            branch_target <= STD_LOGIC_VECTOR(
+                UNSIGNED(NPC) + UNSIGNED(Imm));
+
+        ELSE
+            branch_taken <= '0';
+            branch_target <= (OTHERS => '0');
+        END IF;
+    END PROCESS;
+
+    pipeline_reg : PROCESS (clk)
+    BEGIN
+        IF rising_edge(clk) THEN
+            IF reset = '1' THEN
+                ALUResult_out <= (OTHERS => '0');
+                B_out <= (OTHERS => '0');
+                IR_out <= x"00000013";
+                NPC_out <= (OTHERS => '0');
+                BranchTaken <= '0';
+                BranchTarget <= (OTHERS => '0');
+                MemRead_out <= '0';
+                MemWrite_out <= '0';
+                RegWrite_out <= '0';
+                MemToReg_out <= '0';
+            ELSE
+                ALUResult_out <= alu_result;
+                B_out <= B;
+                IR_out <= IR;
+                NPC_out <= NPC;
+                BranchTaken <= branch_taken;
+                BranchTarget <= branch_target;
+                MemRead_out <= MemRead;
+                MemWrite_out <= MemWrite;
+                RegWrite_out <= RegWrite;
+                MemToReg_out <= MemToReg;
+            END IF;
+        END IF;
+    END PROCESS pipeline_reg;
+
+END rtl;
--- mem_pipeline.vhd
+++ mem_pipeline.vhd
@@ -1,15 +1,198 @@
 LIBRARY ieee;
-USE ieee.std_logic_1164.all;
-USE ieee.numeric_std.all;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
 
+ENTITY mem_stage IS
+    PORT (
+        clk : IN STD_LOGIC;
+        reset : IN STD_LOGIC;
 
-entity mem_pipeline is
-    port(
-        clk : in STD_LOGIC;
-        reset : in STD_LOGIC
+        ALUResult : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        B_in : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        IR_in : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        NPC_in : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+
+        MemRead : IN STD_LOGIC;
+        MemWrite : IN STD_LOGIC;
+        MemFunc : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+        RegWrite : IN STD_LOGIC;
+        MemToReg : IN STD_LOGIC;
+
+        LMD_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+        ALUResult_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+        IR_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+        NPC_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+        RegWrite_out : OUT STD_LOGIC;
+        MemToReg_out : OUT STD_LOGIC
     );
-end mem_pipeline;
+END mem_stage;
 
-architecture rtl of mem_pipeline is
-begin
-end rtl;
+ARCHITECTURE rtl OF mem_stage IS
+
+    COMPONENT memory IS
+        GENERIC (
+            ram_size : INTEGER := 8192;
+            mem_delay : TIME := 1 ns;
+            clock_period : TIME := 1 ns
+        );
+        PORT (
+            clock : IN STD_LOGIC;
+            writedata : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+            address : IN INTEGER RANGE 0 TO 8191;
+            memwrite : IN STD_LOGIC;
+            memread : IN STD_LOGIC;
+            readdata : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+            waitrequest : OUT STD_LOGIC
+        );
+    END COMPONENT;
+
+    CONSTANT MF_BYTE : STD_LOGIC_VECTOR(2 DOWNTO 0) := "000";
+    CONSTANT MF_HALF : STD_LOGIC_VECTOR(2 DOWNTO 0) := "001";
+    CONSTANT MF_WORD : STD_LOGIC_VECTOR(2 DOWNTO 0) := "010";
+    CONSTANT MF_BYTE_U : STD_LOGIC_VECTOR(2 DOWNTO 0) := "100";
+    CONSTANT MF_HALF_U : STD_LOGIC_VECTOR(2 DOWNTO 0) := "101";
+
+    SIGNAL word_addr : INTEGER RANGE 0 TO 8191;
+    SIGNAL byte_offset : STD_LOGIC_VECTOR(1 DOWNTO 0);
+
+    SIGNAL mem_writedata : STD_LOGIC_VECTOR(31 DOWNTO 0);
+    SIGNAL mem_write_en : STD_LOGIC;
+    SIGNAL mem_read_en : STD_LOGIC;
+    SIGNAL mem_readdata : STD_LOGIC_VECTOR(31 DOWNTO 0);
+    SIGNAL mem_waitrequest : STD_LOGIC;
+
+    SIGNAL lmd_comb : STD_LOGIC_VECTOR(31 DOWNTO 0);
+    SIGNAL rmw_word : STD_LOGIC_VECTOR(31 DOWNTO 0);
+
+BEGIN
+
+    word_addr <= TO_INTEGER(UNSIGNED(ALUResult(14 DOWNTO 2)));
+    byte_offset <= ALUResult(1 DOWNTO 0);
+
+    data_mem : memory
+    GENERIC MAP(
+        ram_size => 8192,
+        mem_delay => 1 ns,
+        clock_period => 1 ns
+    )
+    PORT MAP(
+        clock => clk,
+        writedata => mem_writedata,
+        address => word_addr,
+        memwrite => mem_write_en,
+        memread => mem_read_en,
+        readdata => mem_readdata,
+        waitrequest => mem_waitrequest
+    );
+
+    mem_read_en <= MemRead;
+
+    PROCESS (MemWrite, MemFunc, B_in, byte_offset, mem_readdata)
+    BEGIN
+        mem_write_en <= '0';
+        mem_writedata <= (OTHERS => '0');
+        rmw_word <= mem_readdata;
+
+        IF MemWrite = '1' THEN
+            mem_write_en <= '1';
+
+            CASE MemFunc IS
+
+                WHEN MF_BYTE =>
+                    rmw_word <= mem_readdata;
+                    CASE byte_offset IS
+                        WHEN "00" => rmw_word(7 DOWNTO 0) <= B_in(7 DOWNTO 0);
+                        WHEN "01" => rmw_word(15 DOWNTO 8) <= B_in(7 DOWNTO 0);
+                        WHEN "10" => rmw_word(23 DOWNTO 16) <= B_in(7 DOWNTO 0);
+                        WHEN OTHERS => rmw_word(31 DOWNTO 24) <= B_in(7 DOWNTO 0);
+                    END CASE;
+                    mem_writedata <= rmw_word;
+
+                WHEN MF_HALF =>
+                    rmw_word <= mem_readdata;
+                    IF byte_offset(1) = '0' THEN
+                        rmw_word(15 DOWNTO 0) <= B_in(15 DOWNTO 0);
+                    ELSE
+                        rmw_word(31 DOWNTO 16) <= B_in(15 DOWNTO 0);
+                    END IF;
+                    mem_writedata <= rmw_word;
+
+                WHEN OTHERS =>
+                    mem_writedata <= B_in;
+
+            END CASE;
+        END IF;
+    END PROCESS;
+
+    PROCESS (MemRead, MemFunc, mem_readdata, byte_offset)
+        VARIABLE raw_byte : STD_LOGIC_VECTOR(7 DOWNTO 0);
+        VARIABLE raw_half : STD_LOGIC_VECTOR(15 DOWNTO 0);
+    BEGIN
+        lmd_comb <= (OTHERS => '0');
+
+        IF MemRead = '1' THEN
+            CASE MemFunc IS
+
+                WHEN MF_BYTE =>
+                    CASE byte_offset IS
+                        WHEN "00" => raw_byte := mem_readdata(7 DOWNTO 0);
+                        WHEN "01" => raw_byte := mem_readdata(15 DOWNTO 8);
+                        WHEN "10" => raw_byte := mem_readdata(23 DOWNTO 16);
+                        WHEN OTHERS => raw_byte := mem_readdata(31 DOWNTO 24);
+                    END CASE;
+                    lmd_comb <= STD_LOGIC_VECTOR(resize(SIGNED(raw_byte), 32));
+
+                WHEN MF_BYTE_U =>
+                    CASE byte_offset IS
+                        WHEN "00" => raw_byte := mem_readdata(7 DOWNTO 0);
+                        WHEN "01" => raw_byte := mem_readdata(15 DOWNTO 8);
+                        WHEN "10" => raw_byte := mem_readdata(23 DOWNTO 16);
+                        WHEN OTHERS => raw_byte := mem_readdata(31 DOWNTO 24);
+                    END CASE;
+                    lmd_comb <= STD_LOGIC_VECTOR(resize(UNSIGNED(raw_byte), 32));
+
+                WHEN MF_HALF =>
+                    IF byte_offset(1) = '0' THEN
+                        raw_half := mem_readdata(15 DOWNTO 0);
+                    ELSE
+                        raw_half := mem_readdata(31 DOWNTO 16);
+                    END IF;
+                    lmd_comb <= STD_LOGIC_VECTOR(resize(SIGNED(raw_half), 32));
+
+                WHEN MF_HALF_U =>
+                    IF byte_offset(1) = '0' THEN
+                        raw_half := mem_readdata(15 DOWNTO 0);
+                    ELSE
+                        raw_half := mem_readdata(31 DOWNTO 16);
+                    END IF;
+                    lmd_comb <= STD_LOGIC_VECTOR(resize(UNSIGNED(raw_half), 32));
+
+                WHEN OTHERS =>
+                    lmd_comb <= mem_readdata;
+
+            END CASE;
+        END IF;
+    END PROCESS;
+
+    pipeline_reg : PROCESS (clk)
+    BEGIN
+        IF rising_edge(clk) THEN
+            IF reset = '1' THEN
+                LMD_out <= (OTHERS => '0');
+                ALUResult_out <= (OTHERS => '0');
+                IR_out <= x"00000013";
+                NPC_out <= (OTHERS => '0');
+                RegWrite_out <= '0';
+                MemToReg_out <= '0';
+            ELSE
+                LMD_out <= lmd_comb;
+                ALUResult_out <= ALUResult;
+                IR_out <= IR_in;
+                NPC_out <= NPC_in;
+                RegWrite_out <= RegWrite;
+                MemToReg_out <= MemToReg;
+            END IF;
+        END IF;
+    END PROCESS pipeline_reg;
+
+END rtl;
