3a65470212a27716ccb51da0df4303d6ff5c1ad2
Aiden Van Ravenswaay <avanravenswaay@bell.net>
2026-04-14 08:55:06 -0400

    Added some comments
    Updated comments and formatting in instruction_decode.vhd to improve clarity and maintainability.

--- instruction_decode.vhd
+++ instruction_decode.vhd
@@ -8,7 +8,7 @@ entity instruction_decode is
         reset : in STD_LOGIC;
         IR : in STD_LOGIC_VECTOR(31 DOWNTO 0);					-- instruction from IF/ID.IR
         NPC : in STD_LOGIC_VECTOR(31 DOWNTO 0);				-- next PC from IF/ID.NPC
-        -- gets register values passed in now (from register_dile.vhd)
+        -- gets register values passed in now (from register_file.vhd)
         A_in : in STD_LOGIC_VECTOR(31 DOWNTO 0) := (others => '0');
         B_in : in STD_LOGIC_VECTOR(31 DOWNTO 0) := (others => '0');
 
@@ -22,14 +22,14 @@ entity instruction_decode is
         ALUSrc : out STD_LOGIC; -- 0: ALU uses register operand B, 1: ALU uses immediate imm
         ALUOp : out STD_LOGIC_VECTOR(1 DOWNTO 0);
         ALUFunc : out STD_LOGIC_VECTOR(3 DOWNTO 0); -- exact ALU operation select for EX stage
-        Branch : out STD_LOGIC;
-        BranchType : out STD_LOGIC_VECTOR(2 DOWNTO 0);
-        Jump : out STD_LOGIC;
-        JumpReg : out STD_LOGIC;
-        MemRead : out STD_LOGIC;
-        MemWrite : out STD_LOGIC;
-        RegWrite : out STD_LOGIC;
-        MemToReg : out STD_LOGIC
+        Branch : out STD_LOGIC;   --Do we branch or not
+        BranchType : out STD_LOGIC_VECTOR(2 DOWNTO 0);  --Branch type (000 : equal, 001: not equal, 100: less than, 101: greater thn or equal)
+        Jump : out STD_LOGIC;    --If we jump or not  
+        JumpReg : out STD_LOGIC;  --If we jump to an instruction who's address is in a register
+        MemRead : out STD_LOGIC; --If we read data memory
+        MemWrite : out STD_LOGIC; --If we write to data memory
+        RegWrite : out STD_LOGIC; --Wether the instruction write to a DST register
+        MemToReg : out STD_LOGIC --If a we load data from memory into a register
     );
 end instruction_decode;
 
@@ -50,7 +50,6 @@ begin
     funct7 <= IR(31 DOWNTO 25);
 
     -- immediate generation by RV32I format
-    -- !! not sure if im doing the extension properly... sign extend vs zero extend?
     imm_i <= std_logic_vector(resize(signed(IR(31 DOWNTO 20)), 32));
     imm_s <= std_logic_vector(resize(signed(IR(31 DOWNTO 25) & IR(11 DOWNTO 7)), 32));
     imm_b <= std_logic_vector(resize(signed(IR(31) & IR(7) & IR(30 DOWNTO 25) & IR(11 DOWNTO 8) & '0'), 32));
